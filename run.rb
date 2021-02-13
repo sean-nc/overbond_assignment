ca5b7c94195e49c667bc3cc324d39678bc1236de
Sean <sean.n.cordes@gmail.com>
2021-02-13 17:18:55 -0500

    changed the wording for the selections in run.rb

--- run.rb
+++ run.rb
@@ -6,17 +6,17 @@ files = {}
 Dir.glob('*.csv').each_with_index { |filename, i| files[i] = filename }
 
 functions = {
-  0 => 'spread_to_curve',
-  1 => 'spread_to_benchmark'
+  0 => 'spread_to_benchmark',
+  1 => 'spread_to_curve'
 }
 
 file_num = -1
 
 until files.keys.include?(file_num)
-  puts 'select sample data (default: 0)'
+  puts "Enter the number of the data you'd like to select:\n"
 
   files.each do |k, v|
-    puts "#{k}: #{v}"
+    puts "#{k}: #{v}#{' <- default' if k == 0}"
   end
 
   file_num = gets.to_i
@@ -27,10 +27,10 @@ puts "You selected '#{files[file_num]}'\n\n"
 function_num = -1
 
 until functions.keys.include?(function_num)
-  puts 'select sample data (default: 0)'
+  puts "Enter the number of the function you'd like to select:\n"
 
   functions.each do |k, v|
-    puts "#{k}: #{v}"
+    puts "#{k}: #{v}#{' <- default' if k == 0}"
   end
 
   function_num = gets.to_i
